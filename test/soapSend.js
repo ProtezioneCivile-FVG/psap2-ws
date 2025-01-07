d675fe70f80c3fb1574ff4a43c9f62169a518dfe
Marco Foschian <marco.foschian@protezionecivile.fvg.it>
2025-01-07 12:30:07 +0100

    SOAP message switched to regular instead of "Beta80"

--- test/soapSend.js
+++ test/soapSend.js
@@ -37,17 +37,20 @@ soap.createClient(url, function(err, client) {
 		 console.error( 'Error: %s', err );
 		 return;
 	}
-	// console.log( "client.describe: %s", util.inspect(client.describe()) );
-	// let parms = {
-	// 	// SchedaContatto: cdataOf(xml)
-	// 	// SchedaContatto: escapeCDATA(xml)
-	// 	SchedaContatto: xml
-	// };
-	let parms = { parameters : [
-		{item: {key: "SchedaContatto", value: xml }},
-		{item: {key: "EnteMittente", value: "NUE" }},
-		{item: {key: "ProvinciaMittente", value: "UD" }},
-	]};
+	console.log( "client.describe: %s", util.inspect(client.describe()) );
+	let parms = {
+		// SchedaContatto: cdataOf(xml)
+		// SchedaContatto: escapeCDATA(xml)
+		SchedaContatto: xml,
+		EnteMittente: "NUE",
+		ProvinciaMittente: "UD"
+	};
+	// Beta80 Wrong Data Invocation Test
+	// let parms = { parameters : [
+	// 	{item: {key: "SchedaContatto", value: xml }},
+	// 	{item: {key: "EnteMittente", value: "NUE" }},
+	// 	{item: {key: "ProvinciaMittente", value: "UD" }},
+	// ]};
 
 	// console.log(parms.SchedaContatto);
 
