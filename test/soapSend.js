21b683094562360bd4d59a615d05dfac98668d8d
Marco Foschian <marco.foschian@protezionecivile.fvg.it>
2025-01-07 12:01:14 +0100

    bugfix in testing scripts

--- test/soapSend.js
+++ test/soapSend.js
@@ -3,8 +3,6 @@ const util = require('util');
 
 let url = 'http://localhost:8001/Nue_Services/EntiService?wsdl';
 
-//let args = {SchedaContatto: 'Ciao, sono una scheda contatto'};
-
 
 let args = process.argv.splice(2);
 let xml_file = args[0];
@@ -17,6 +15,11 @@ if( !xml_file ) {
 console.log( 'Reading xml from %s', xml_file );
 let xml = require('fs').readFileSync(xml_file, 'utf8');
 
+const server = args[1];
+if( server )
+	url = url.replace('localhost', server);
+
+
 function escapeCDATA(xml) {
 	let regex = /\]\]>/g;
 	let res = xml.replace(regex, "]]]]><![CDATA[>]]>");
@@ -26,8 +29,6 @@ function cdataOf(xml) {
 	return "<![CDATA[" + escapeCDATA(xml) + "]]>";
 }
 
-let server = args[1] || "psap2ws.protezionecivile.fvg.it";
-url = url.replace('localhost', server);
 
 console.log( 'Parsing wsdl from %s', url );
 
